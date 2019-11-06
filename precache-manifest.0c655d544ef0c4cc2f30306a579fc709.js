a39b940d30180cbc2428c8b48d779aa93d629379
OlgaSavytska <o.savytska77@gmail.com>
2019-11-06 15:04:04 +0200

    Updates

---
+++ precache-manifest.0c655d544ef0c4cc2f30306a579fc709.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "845b2efbdb5c2f24931ad6d581c9ddd7",
+    "url": "/test_os_01/index.html"
+  },
+  {
+    "revision": "d3209412cdd92a83c844",
+    "url": "/test_os_01/static/css/main.68aadba0.chunk.css"
+  },
+  {
+    "revision": "1161df167990f59d3910",
+    "url": "/test_os_01/static/js/2.06b41e7f.chunk.js"
+  },
+  {
+    "revision": "d3209412cdd92a83c844",
+    "url": "/test_os_01/static/js/main.a2162157.chunk.js"
+  },
+  {
+    "revision": "5112701e1e5860a7aa0c",
+    "url": "/test_os_01/static/js/runtime-main.61bd8ccd.js"
+  },
+  {
+    "revision": "89314d3cbe24db4aefd9f4b63bb56da3",
+    "url": "/test_os_01/static/media/Background.89314d3c.png"
+  }
+]);
